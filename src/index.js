a1ca6720bd34581cfa913b4d2a695ef0ece4130a
Gulashan Hashami <gulashan959898@gmail.com>
2022-05-26 08:09:17 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express=require("express");
 const {register, login}=require("./controllers/user.controller");
 const productController=require("./controllers/product.controller")
+const cartController=require("./controllers/cart.controller")
+const addressController=require("./controllers/address.controller")
 
 const app=express();
 app.use(express.json());
 app.post("/register", register); 
 app.post("/login", login);
 app.use("/products", productController);
+app.use("/carts", cartController);
+app.use("/address", addressController);
 
 module.exports=app; 
---
+++ src/models/cart.model.js
@@ -0,0 +1,18 @@
+
+
+const mongoose=require("mongoose");
+const cartSchema=new mongoose.Schema(
+    {
+        title: { type: String, require: true },
+        price: { type: Number, require: true },
+        category: { type: String, require: true },
+        description: { type: String, require: true },
+        image: { type: String, require: true }, 
+        rate: { type: Number, require: true }, 
+    },
+    {
+        versionKey:false,
+        timestamps:true,
+    }
+);
+module.exports= mongoose.model("cart", cartSchema);
