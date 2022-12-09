b35924fd6d91e18b11a7c14653ba37b4e332c144
Gulashan Hashami <gulashan959898@gmail.com>
2022-12-09 23:42:42 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-const express=require("express");
+
+const express = require('express');
+const app = express();
+const connect = require('./configs/db');
+const cors = require('cors'); 2
 const {register, login}=require("./controllers/user.controller");
 const productController=require("./controllers/product.controller")
 const cartController=require("./controllers/cart.controller")
 const addressController=require("./controllers/address.controller")
-
-const app=express();
 app.use(express.json());
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 app.post("/register", register); 
 app.post("/login", login);
 app.use("/products", productController);
 app.use("/carts", cartController);
 app.use("/address", addressController);
 
-module.exports=app; 
+
+const port = process.env.PORT || 2345;
+app.listen(port, async () => {
+  try {
+    await connect();
+  } catch (err) {
+    console.error(err.message);
+  }
+  console.log(`listening on port ${port}`);
+});
